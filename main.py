22a0f8472c0f06319aae2a4cf94a3e323bb52b91
AlexNovikovRussian <alexprogramer@mail.ru>
2020-12-10 22:13:51 +0300

    full realisation of mute

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pyowm.utils.config import get_default_config
 import requests
 import json
 import datetime
+from discord.utils import get
 
 
 config_dict = get_default_config()
@@ -16,10 +17,16 @@ mgr = owm.weather_manager()
 TARGET = 3
 PREFIX = "%"
 
-mute_nick = None
-mute_msg = None
-muting = False
-mute_reacts = [0,0]
+class Voicing():
+    def __init__(self, message,member,state):
+        self.message = message
+        self.member = member
+        self.state = state
+        self.positive_reactions = 0
+        self.negative_reactions = 0
+
+
+mute = Voicing(None,None,None)
 
 class Bot(discord.Client):
     async def on_ready(self):
@@ -28,7 +35,7 @@ class Bot(discord.Client):
     async def on_message(self, message):
         args = message.content.split(" ")
         args.remove(args[0])
-        if message.content.startswith(PREFIX+"cupdo" or PREFIX+"сгзвщ" or PREFIX+"капдо"):
+        if message.content.lower().startswith(PREFIX+"cupdo" or PREFIX+"сгзвщ" or PREFIX+"капдо"):
             string = ""
             exstring=""
             is_up=False
@@ -49,7 +56,7 @@ class Bot(discord.Client):
             #await ctx.send(content="From {0}({1})\n{2}".format(ctx.message.author.display_name, ctx.message.author, exstring))
             await message.channel.send(embed=discord.Embed(description=exstring).set_author(name="{0}({1})".format(message.author.display_name, message.author),icon_url=message.author.avatar_url))
         
-        elif message.content.startswith(PREFIX+"погода" or PREFIX+"пагода" or PREFIX+"цуферук" or PREFIX+"weather"):
+        elif message.content.lower().startswith(PREFIX+"погода" or PREFIX+"пагода" or PREFIX+"цуферук" or PREFIX+"weather"):
             try:
                 weather = mgr.weather_at_place(args[0]).weather
             except:
@@ -59,7 +66,7 @@ class Bot(discord.Client):
             print(temp)
             await message.channel.send("Температура: " + str(temp["temp"]) + "\nМинимальная температура: " + str(temp["temp_min"]) + "\nМаксимальная температура: " + str(temp["temp_max"]) + "\nОщущается как: " + str(temp["feels_like"]))
         
-        elif message.content.startswith(PREFIX+"rub"):
+        elif message.content.lower().startswith(PREFIX+"rub"):
             await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name = "за рублём"))
             if len(args) == 0:
                 rate = json.loads(requests.get("https://rubisintheass.firebaseio.com/RUB.json").text)["USD"]
@@ -73,7 +80,7 @@ class Bot(discord.Client):
             except KeyError:
                 await message.channel.send(embed=discord.Embed(title="404 - " + arg1, description="Такая валюта не найдена", timestamp=datetime.datetime.now()))
 
-        elif message.content.startswith(PREFIX+"reverse"):
+        elif message.content.lower().startswith(PREFIX+"reverse"):
             string = ""
             for st in args:
                 string +=st+" "
@@ -84,7 +91,56 @@ class Bot(discord.Client):
 
             await message.delete()
             await message.channel.send(embed=discord.Embed(description=ex).set_author(name="{0}({1})".format(message.author.display_name, message.author),icon_url=message.author.avatar_url))
+        
+        elif message.content.lower().startswith(PREFIX+"mute"):
+            if mute.state == True:
+                await message.channel.send("Процесс голосования уже запущен, завершите его для начала следущего")
+                return
+            mentions = message.mentions
+            try:
+                assert len(mentions) ==1
+            except:
+                await message.channel.send("Вы не указали пользователя")
+                return
             
+            mute.member = mentions[0]
+            del mentions
+            if mute.member.bot == True:
+                await message.channel.send("Боты - это исчезающий вид, НЕ УБИВАЙТЕ БЕЗЗАЩИТНЫХ!")
+                return
+            mute.message = await message.channel.send("Замутить **"+mute.member.display_name+"** Кто за, ставьте <:correct:784434578424725535>, если нет, то <:incorrect:714834242525331498>")
+            mute.state = True
+            await mute.message.add_reaction("<:correct:784434578424725535>")
+            await mute.message.add_reaction("<:incorrect:714834242525331498>")
+
+    async def on_reaction_add(self,reaction, user):
+        if reaction.message == mute.message:
+            if str(reaction.emoji.url) == "https://cdn.discordapp.com/emojis/784434578424725535.png": #positive
+                mute.positive_reactions += 1
+                
+                if mute.positive_reactions >= TARGET:
+                    await mute.message.delete()
+                    role = get(reaction.message.guild.roles, name="банка")
+                    await mute.member.add_roles(role, reason="голосование")
+                    mute.member = None
+                    mute.message = None
+                    mute.state = False
+                    mute.positive_reactions = 0
+                    mute.negative_reactions = 0
+                    await reaction.message.channel.send("mute")
+                
+            elif str(reaction.emoji.url) == "https://cdn.discordapp.com/emojis/714834242525331498.png": #negative
+                mute.negative_reactions += 1
+
+                if mute.negative_reactions >= TARGET:
+                    await mute.message.delete()
+                    mute.member = None
+                    mute.message = None
+                    mute.state = False
+                    mute.positive_reactions = 0
+                    mute.negative_reactions = 0
+                    await reaction.message.channel.send("no mute")
+
 bot = Bot()
 bot.run("NzY4MDI4Mzk5MjEzNDc3ODkw.X46gLw.xiXUtgD8nmLJPNUP4YY2_r_goTc")
 
