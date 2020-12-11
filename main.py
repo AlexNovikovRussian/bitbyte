061c7b8d4ee63a8e2e94758c253b533571e2651b
AlexNovikovRussian <alexprogramer@mail.ru>
2020-12-11 22:59:27 +0300

    realise unmute command

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ config_dict['language'] = 'ru'
 owm = OWM('d5cefe4e77e8e08ca203715b718cddde', config_dict)
 mgr = owm.weather_manager()
 
-TARGET = 3
+TARGET = 2
 PREFIX = "%"
 
 class Voicing():
@@ -27,6 +27,7 @@ class Voicing():
 
 
 mute = Voicing(None,None,None)
+unmute = Voicing(None,None,None)
 
 class Bot(discord.Client):
     async def on_ready(self):
@@ -108,10 +109,35 @@ class Bot(discord.Client):
             if mute.member.bot == True:
                 await message.channel.send("Боты - это исчезающий вид, НЕ УБИВАЙТЕ БЕЗЗАЩИТНЫХ!")
                 return
+            if get(mute.member.guild.roles, name="банка") in mute.member.roles:
+                await message.channel.send("Замутить замученного? Не справедливо!")
+                return
             mute.message = await message.channel.send("Замутить **"+mute.member.display_name+"** Кто за, ставьте <:correct:784434578424725535>, если нет, то <:incorrect:714834242525331498>")
             mute.state = True
             await mute.message.add_reaction("<:correct:784434578424725535>")
             await mute.message.add_reaction("<:incorrect:714834242525331498>")
+        
+        elif message.content.lower().startswith(PREFIX+"unmute"):
+            if unmute.state == True:
+                await message.channel.send("Процесс голосования уже запущен, завершите его для начала следущего")
+                return
+            mentions = message.mentions
+            try:
+                assert len(mentions) ==1
+            except:
+                await message.channel.send("Вы не указали пользователя")
+                return
+            
+            if not get(mentions[0].guild.roles, name="банка") in mentions[0].roles:
+                await message.channel.send("Человек не замьючен")
+                return
+            unmute.member = mentions[0]
+            del mentions
+
+            unmute.message = await message.channel.send("Размутить **"+unmute.member.display_name+"** Кто за, ставьте <:correct:784434578424725535>, если нет, то <:incorrect:714834242525331498>")
+            unmute.state = True
+            await unmute.message.add_reaction("<:correct:784434578424725535>")
+            await unmute.message.add_reaction("<:incorrect:714834242525331498>")
 
     async def on_reaction_add(self,reaction, user):
         if reaction.message == mute.message:
@@ -120,6 +146,14 @@ class Bot(discord.Client):
                 
                 if mute.positive_reactions >= TARGET:
                     await mute.message.delete()
+
+                    role_ids = [str(r.id) for r in mute.member.roles]
+                    rls = ":".join(role_ids)
+                    req = {str(mute.member.id): rls}
+                    requests.patch("https://rubisintheass.firebaseio.com/MUTES.json", data=json.dumps(req))
+
+                    await mute.member.edit(roles=[])
+
                     role = get(reaction.message.guild.roles, name="банка")
                     await mute.member.add_roles(role, reason="голосование")
                     mute.member = None
@@ -140,6 +174,38 @@ class Bot(discord.Client):
                     mute.positive_reactions = 0
                     mute.negative_reactions = 0
                     await reaction.message.channel.send("no mute")
+        elif reaction.message == unmute.message:
+            if str(reaction.emoji.url) == "https://cdn.discordapp.com/emojis/784434578424725535.png": #positive
+                unmute.positive_reactions += 1
+                
+                if unmute.positive_reactions >= TARGET:
+                    await unmute.message.delete()
+
+                    user = json.loads(requests.get("https://rubisintheass.firebaseio.com/MUTES.json").text)[str(unmute.member.id)]
+
+                    rls = user.split(":")
+                    roles = [get(unmute.member.guild.roles, id=int(r)) for r in rls]
+
+                    await unmute.member.edit(roles=roles)
+
+                    unmute.member = None
+                    unmute.message = None
+                    unmute.state = False
+                    unmute.positive_reactions = 0
+                    unmute.negative_reactions = 0
+                    await reaction.message.channel.send("unmute")
+                
+            elif str(reaction.emoji.url) == "https://cdn.discordapp.com/emojis/714834242525331498.png": #negative
+                unmute.negative_reactions += 1
+
+                if unmute.negative_reactions >= TARGET:
+                    await unmute.message.delete()
+                    unmute.member = None
+                    unmute.message = None
+                    unmute.state = False
+                    unmute.positive_reactions = 0
+                    unmute.negative_reactions = 0
+                    await reaction.message.channel.send("no unmute")
 
 bot = Bot()
 bot.run("NzY4MDI4Mzk5MjEzNDc3ODkw.X46gLw.xiXUtgD8nmLJPNUP4YY2_r_goTc")
